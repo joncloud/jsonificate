a046ef2cc9327b2efd8ede73e337e7af4af388a3
joncloud <jdberube@gmail.com>
2021-01-25 21:41:25 -0800

    Ignores code coverage

--- src/Jsonificate/ReflectionReferenceAttribute.cs
+++ src/Jsonificate/ReflectionReferenceAttribute.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Jsonificate
 {
+    [ExcludeFromCodeCoverage]
     class ReflectionReferenceAttribute : Attribute
     {
         public ReflectionReferenceAttribute(Type type) { }
